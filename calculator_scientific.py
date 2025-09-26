763be8d9c88f4d58decdcbdbbd63b5bf19727f0e
Harsh2594 <harsh.sgm13@gmail.com>
2025-09-26 21:38:56 +0530

    Add scientific operations (power, square root)

---
+++ calculator_scientific.py
@@ -0,0 +1,11 @@
+import math
+
+def power(a, b):
+    return a ** b
+
+def square_root(a):
+    return math.sqrt(a)
+
+if __name__ == "__main__":
+    print("Power:", power(2, 3))        
+    print("Square root:", square_root(16))  
