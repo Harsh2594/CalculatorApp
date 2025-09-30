398bf4b8808dd737138674049f336a073f32224e
Harsh2594 <harsh.sgm13@gmail.com>
2025-09-30 11:36:44 +0530

    Fix ValueError in divide func

--- calculator_basic.py
+++ calculator_basic.py
@@ -18,7 +18,7 @@ def multiply(a, b):
 
 def divide(a, b):
     if b == 0:
-        return "Cannot divide by zero"
+        raise ValueError("Division by zero is not allowed")
     return a / b
 
 def modulus(a, b):
