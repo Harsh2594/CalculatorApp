afd294ce2b41aae9114fdd35d14519465c02d54c
Harsh2594 <harsh.sgm13@gmail.com>
2025-09-30 16:34:00 +0530

    remove 2 blank lines

--- calculator_basic.py
+++ calculator_basic.py
@@ -10,23 +10,15 @@ def multiply(a, b):
     print(f"Main branch multiplying {a} and {b}")
     print(f"Feature branch multiplying {a} and {b}")
     return a * b
-
 #divide two numbers
-
-
-
-
 def divide(a, b):
     if b == 0:
         raise ValueError("Division by zero is not allowed")
     return a / b
-
 def modulus(a, b):
     if b == 0:
         raise ValueError("Cannot modulus by zero")
     return a % b
-
-
 if __name__ == "__main__":
     print("Add:", add(10, 5))
     print("Subtract:", subtract(10, 5))
