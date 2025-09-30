2371b0e7d3020044c84a15c27a37126a314c1f15
Harsh2594 <harsh.sgm13@gmail.com>
2025-09-30 18:33:03 +0530

    Remove unnecessary comment

--- calculator_basic.py
+++ calculator_basic.py
@@ -1,18 +1,18 @@
 # calculator_basic.py
 
 
-#Add two numbers
+
 def add(a, b):
     return a + b
-#subtract two numbers
+
 def subtract(a, b):
     return a - b
-#multiply two numbers
+
 def multiply(a, b):
     print(f"Main branch multiplying {a} and {b}")
     print(f"Feature branch multiplying {a} and {b}")
     return a * b
-#divide two numbers
+
 def divide(a, b):
     if b == 0:
         raise ValueError("Division by zero is not allowed")
