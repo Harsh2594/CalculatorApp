864f7c49b1797436e7bb8e99a76abde5daf303ed
Harsh2594 <harsh.sgm13@gmail.com>
2025-09-30 18:37:12 +0530

    Remove unnecessary space between lines

--- calculator_basic.py
+++ calculator_basic.py
@@ -1,26 +1,29 @@
-# calculator_basic.py
-
-
-
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
+
 
 def multiply(a, b):
     print(f"Main branch multiplying {a} and {b}")
     print(f"Feature branch multiplying {a} and {b}")
     return a * b
 
+
 def divide(a, b):
     if b == 0:
         raise ValueError("Division by zero is not allowed")
     return a / b
+
+
 def modulus(a, b):
     if b == 0:
         raise ValueError("Cannot modulus by zero")
     return a % b
+
+
 if __name__ == "__main__":
     print("Add:", add(10, 5))
     print("Subtract:", subtract(10, 5))
